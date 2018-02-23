678c56cbb27a907a9e0d039aa7c3a6273f23e88e
Bence Lovas <bansoul1@gmail.com>
2018-02-23 20:05:55 +0100

    login & registration forms

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule
