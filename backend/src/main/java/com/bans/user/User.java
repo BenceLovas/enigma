f9f59afa70c4f47974af6e2efea559f09648498d
Bence Lovas <bansoul1@gmail.com>
2018-03-08 22:29:12 +0100

    @back - added size constraints to user model's name & password

--- backend/src/main/java/com/bans/user/User.java
+++ backend/src/main/java/com/bans/user/User.java
@@ -20,11 +20,11 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
-    // TODO add length of hash as size
+    @Size(min = 5, max = 30)
     private String name;
 
     @Column(nullable = false)
-    @Size(min = 8, max = 20)
+    @Size(min = 60, max = 60)
     private String password;
 
     @Column(nullable = false, unique = true)
